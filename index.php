77e6fe74314f5c56b211161f00bf30580a582ea1
Dayane <nikolpuscan@gmail.com>
2024-10-03 00:53:40 -0500

    ejemplo_pagina

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=resources\css\bootstrap.min.css>
+    <link rel="stylesheet" href="resources\css\bootstrap-icons\font\bootstrap-icons.css">
+    <link rel="stylesheet" href="resources\css\main.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center"> EN LINEA </h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right  pr-5"></p>
+            SOLUCIONES MUNDIALES
+        </div>
+    </div>
+    <!-- encabezado alt+shif A-->
+    <div class="row">
+        <div class="col-md-12">
+            <img class="w-100" style="height: 350px;" src="resources/img/baner01.PNG" alt="">
+        </div>
+    </div>
+    <!-- pie de encabezado -->
+    <div class="container border border-danger mt-5">
+        <h1> SOLUCIONES CREATIVAS </h1>
+        <p> Sin importar cual sea tu negocio, nuestro equipo puede hacer todo desde cero</p>
+    </div>
+    <!-- IMAGENES -->
+
+        <div class="row mt-4">
+            <div class="col-sm-12 col-md-4 border boder-danger">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources\img\idea_01.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                            <i class="bi bi-pencil-square"></i>DISEÑO</h5>
+                        <P>CREAMOS DISEÑOS SEGUN TUS NESECIDADES</P>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 border boder-danger">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources\img\idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                            <i class="bi bi-flower3">
+                        </i>DESARROLLO</h5>
+                        <P>Nuestros programadores te garantizan la perfeccion</P>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 border boder-danger">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources\img\idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                            <i class="bi bi-file-plus">
+                            </i>EDICION DE LIBROS</h5>
+                        <P>Que el producto final llegue a tus clientes</P>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-wine">
+        <p class="p-3">Edificio Las Hadas. Guadalajara www sitioincreible.mx (33) 3214 6547</p>
+    </footer>
+
+
+</body>
+
+</html>
+
+</body>
+
+</html>
